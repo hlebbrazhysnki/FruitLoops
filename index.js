b0b698565e5a9a30d3fc0876b5329c4e0d16fac3
Hleb Brazhynski <hleb200217@gmail.com>
2021-10-19 06:42:25 -0400

    LoopsHW

---
+++ index.js
@@ -0,0 +1,31 @@
+//first example.
+
+var myArray = [];
+for (var i = 1; i <= 5; i++) {
+   myArray.push(i);
+}
+
+//second example.
+
+var myArray = [];
+var i = 5;
+while (i >= 0){
+    myArray.push(i);
+  i--;
+}
+
+//third example
+
+var myArray = [];
+for (var i = 1; i < 10; i += 2){
+  myArray.push(i);
+}
+
+
+//final example
+
+var myArray = [];
+for (var i = 9; i > 0; i -= 2){
+  myArray.push(i);
+}
+
